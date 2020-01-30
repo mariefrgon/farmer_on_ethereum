bb3cccd8042f0aca471e58f21ddf215e46b11dfe
BSalliot <berenice.salliot@gmail.com>
2020-01-30 19:27:09 +0100

    resolved conflicts : added function refund

--- contracts/solidity/FarmingInvestment.sol
+++ contracts/solidity/FarmingInvestment.sol
@@ -6,29 +6,34 @@ contract mortal {
     function kill() public { if (msg.sender == farmer) selfdestruct(farmer); }
     
     //console.log
-    event LogUint(string message, uint number);
-    function log(string memory s , uint x) public {
+    event LogUint(string, uint);
+    function log(string memory s , uint x) internal {
        emit LogUint(s, x);
     }
     
-    event LogInt(string message, int number);
-    function log(string memory s , int x) public {
+    event LogInt(string, int);
+    function log(string memory s , int x) internal {
         emit LogInt(s, x);
     }
 
     
-    event LogBytes32(string message, bytes32 number);
-    function log(string memory s , bytes32 x) public {
+    event LogBytes(string, bytes);
+    function log(string memory s , bytes x) internal {
+        emit LogBytes(s, x);
+    }
+    
+    event LogBytes32(string, bytes32);
+    function log(string memory s , bytes32 x) internal {
         emit LogBytes32(s, x);
     }
 
-    event LogAddress(string message, address id);
-    function log(string memory s , address x) public {
+    event LogAddress(string, address);
+    function log(string memory s , address x) internal {
         emit LogAddress(s, x);
     }
 
-    event LogBool(string message, bool state);
-    function log(string memory s , bool x) public {
+    event LogBool(string, bool);
+    function log(string memory s , bool x) internal {
         emit LogBool(s, x);
     }
 }
@@ -87,7 +92,6 @@ contract farminginvestment is mortal {
             collectedAmount + _investedAmount <= goalAmount,
             "Contract over invested."
         );
-
         // Si tout va bien jusque la, on accepte l'investisseur donc creation de l'investisseur
         investors.push(Investor({investedAmount:_investedAmount, id:msg.sender}));
 
@@ -98,17 +102,18 @@ contract farminginvestment is mortal {
 
         // End ?
         if(collectedAmount == goalAmount){
-            investmentPeriodEnd();
+            goalAmountReached();
             log("reached goal: ", collectedAmount);
         }
+        
     }
 
-    function investmentPeriodEnd () internal {
+    function goalAmountReached() internal {
         require(
             collectedAmount == goalAmount,
              "Goal amount of money has not yet been collected"
         );
-
+        
         cowBreeder.transfer(goalAmount);
         cowsBought = true;
         log("cows bought: ", cowsBought);
@@ -137,4 +142,16 @@ contract farminginvestment is mortal {
             log("return on investment: ", part);
         }
     }
+    
+    function refund() public{
+        if(now > limitDate){
+            for (uint i = 0; i < nbInvestors; i++){
+                investors[i].id.transfer(investors[i].investedAmount);
+                log("returned investedAmount: ", investors[i].investedAmount);
+            }
+
+        }
+        
+    }
+    
 }
